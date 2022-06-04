3a2245d6895961ee9cf0e06ecd475f45e7333d1f
AkowsS <ailton.gv@hotmail.com>
2022-06-04 15:24:00 -0300

    atualização

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,20 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./App.css";
-import SearchResults from "./SearchResults/";
+import SearchResults from "./index";
 
 //https://hn.algolia.com/api/v1/search?query=chave
 function App() {
-
   const[hits,setHits] = useState([]);
 
-  const handleInputChange = (e:any) =>{
+  useEffect(()=>{
+    const url = "https://hn.algolia.com/api/v1/search?query=";
+    
+    fetch(url)
+    .then((response) => response.json())
+    .then((hits) => setHits(hits));
+  },[]);
+
+  function handleInputChange(e:any){
     const {value} = e.target;
 
     const url = `https://hn.algolia.com/api/v1/search?query=${value}`;
@@ -21,8 +28,8 @@ function App() {
     <div className="App">
       <h1>Biblioteca</h1>
       <div className="separator">
-        <form>
-          <input type="text" id="search" placeholder="Informe a pesquisa..." onChange={handleInputChange}/>
+        <form >
+          <input type="text" placeholder="Informe a pesquisa..."  onChange={(event) => handleInputChange(event)}/>
         </form>
       </div>
       <SearchResults data = {hits}/>
